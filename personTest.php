bb598f5534550ec1295e006f9fb165bcaac49b04
mint <michaelazona@gmail.com>
2016-06-13 13:38:56 -0500

    Added tests and class

---
+++ personTest.php
@@ -0,0 +1,20 @@
+<?php
+
+	require_once 'person.php';
+
+	class PersonTest extends PHPUNIT_Framework_TestCase
+	{
+		public $test;
+
+		public function setUp()
+		{
+			$this->test = new Person("Mike");
+		}
+
+		public function testName()
+		{
+			$mike = $this->test->getName();
+			$this->assertTrue($mike == "Mike");
+		}
+	}
+?>
